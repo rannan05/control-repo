fbe0d9a940d59d331ea3e750822156d7e11684c4
rannan05 <33558710+rannan05@users.noreply.github.com>
2017-11-29 15:41:18 -0500

    Update Windows_Baseline.pp

--- site/profile/manifests/Windows_Baseline.pp
+++ site/profile/manifests/Windows_Baseline.pp
@@ -1,3 +1,14 @@
 class profile::Windows_Baseline {
 
+  include chocolatey
+  
+  package { 'notepadplusplus':
+    ensure   => installed,
+    provider => 'chocolatey',
+  }
+
+  package { 'jre8':
+    ensure   => installed,
+    provider => 'chocolatey',
+  }
 }
