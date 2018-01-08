e0d99f1f605e3c264f0140f68c9cf08c02d84815
rannan05 <33558710+rannan05@users.noreply.github.com>
2018-01-08 11:20:40 -0500

    Update linux_webserver.pp

--- site/profile/manifests/linux_webserver.pp
+++ site/profile/manifests/linux_webserver.pp
@@ -1,4 +1,14 @@
 class profile::linux_webserver {
 # To install Apache, Java, open firewall port 80, and deploy sample website
 
+  rhsm_register { 'subscription.rhn.example.com':
+    username        => 'myusername',
+    password        => 'mypassword',
+    pool		        => 'mypoolid',
+  }
+  
+  package { 'httpd24-httpd':
+    ensure => present,
+  }
+
 }
